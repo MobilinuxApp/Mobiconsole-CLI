92abce3427c007428513cc2c2e10381d1516d340
Chinmay Gokhale <64396758+MobilinuxApp@users.noreply.github.com>
2020-06-15 17:29:28 +0530

    Update ubuntu_eoan_mate.sh

--- Distribution/Ubuntu/Installer/Suites/Eoan/ubuntu_eoan_mate.sh
+++ Distribution/Ubuntu/Installer/Suites/Eoan/ubuntu_eoan_mate.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 folder=ubuntu-fs
 pkg install dialog
-dialog --title "Storage Info" --msgbox "\n\nCustom Debian Installation would occupy around 2GB of space on your device as per your Desktop choice.\n\nIf you wish to Quit right now press Ctrl+C\n\n Press OK to Continue." 20 40
+dialog --title "Storage Info" --msgbox "\n\nCustom Ubuntu Installation would occupy around 2GB of space on your device as per your Desktop choice.\n\nIf you wish to Quit right now press Ctrl+C\n\n Press OK to Continue." 20 40
 dlink="https://raw.githubusercontent.com/MobilinuxApp/Mobiconsole-CLI/master/Distribution/Ubuntu"
 if [ -d "$folder" ]; then
 	first=1
