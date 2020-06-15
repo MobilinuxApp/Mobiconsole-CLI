1e28cf34644f99a3156a9a29ebd9942c65df8a6c
Chinmay Gokhale <64396758+MobilinuxApp@users.noreply.github.com>
2020-06-15 18:53:39 +0530

    Update kali_uninstall.sh

--- Distribution/KaliLinux/Uninstaller/kali_uninstall.sh
+++ Distribution/KaliLinux/Uninstaller/kali_uninstall.sh
@@ -7,12 +7,20 @@ rm -rf kali-fs
 rm -rf kali-binds
 rm -rf kali.sh
 rm -rf start-kali.sh
-rm -rf ssh-apt.sh
-rm -rf de-apt.sh
-rm -rf de-apt-xfce4.sh
-rm -rf de-apt-mate.sh
-rm -rf de-apt-lxqt.sh
-rm -rf de-apt-lxde.sh
+rm -rf kali-ssh.sh
+rm -rf kali_lxde_de.sh
+rm -rf kali_lxqt_de.sh
+rm -rf kali_mate_de.sh
+rm -rf kali_xfce4_de.sh
+rm -rf kali_awesome_de.sh
+rm -rf kali_icewm_de.sh
+rm -rf kali_custom.sh
+rm -rf kali_custom_awesome.sh
+rm -rf kali_custom_icewm.sh
+rm -rf kali_custom_lxde.sh
+rm -rf kali_custom_lxqt.sh
+rm -rf kali_custom_mate.sh
+rm -rf std-kali-installer.sh
 rm -rf kali_uninstall.sh
 
 echo "Done"
