bdc61affffe6e6c24b960fd4d6e4749ef68b5cef
Chinmay Gokhale <64396758+MobilinuxApp@users.noreply.github.com>
2020-06-17 17:41:37 +0530

    Update backbox_uninstall.sh

--- Distribution/BackBox/Uninstaller/backbox_uninstall.sh
+++ Distribution/BackBox/Uninstaller/backbox_uninstall.sh
@@ -7,12 +7,20 @@ rm -rf backbox-fs
 rm -rf backbox-binds
 rm -rf backbox.sh
 rm -rf start-backbox.sh
-rm -rf ssh-apt.sh
-rm -rf de-apt.sh
-rm -rf de-apt-xfce4.sh
-rm -rf de-apt-mate.sh
-rm -rf de-apt-lxqt.sh
-rm -rf de-apt-lxde.sh
+rm -rf backbox_lxde_de.sh
+rm -rf backbox_lxqt_de.sh
+rm -rf bockbox_mate_de.sh
+rm -rf backbox_xfce4_de.sh
+rm -rf backbox-ssh.sh
+rm -rf backbox_awesome_wm.sh
+rm -rf backbox_icewm_wm.sh
+rm -rf backbox_custom.sh
+rm -rf backbox_custom_awesome.sh
+rm -rf backbox_custom_icewm.sh
+rm -rf backbox_custom_lxde.sh
+rm -rf backbox_custom_lxqt.sh
+rm -rf backbox_custom_mate.sh
+rm -rf std-backbox-installer.sh
 rm -rf backbox_uninstall.sh
 
 echo "Done"
