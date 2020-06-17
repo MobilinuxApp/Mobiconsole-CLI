69a657d79c3a25a03c08ef47b17ea0fba61fd995
Chinmay Gokhale <64396758+MobilinuxApp@users.noreply.github.com>
2020-06-17 17:01:51 +0530

    Update bockbox_mate_de.sh

--- Distribution/BackBox/Installer/DEs/MATE/bockbox_mate_de.sh
+++ Distribution/BackBox/Installer/DEs/MATE/bockbox_mate_de.sh
@@ -3,8 +3,41 @@
 #Get the necessary components
 apt-mark hold udisks2
 [ ! -f /root/.parrot ] && apt-get update || echo "Parrot detected, not updating apt cache since that will break the whole distro"
-apt-get install mate-desktop-environment-core mate-terminal tightvncserver -y
-apt-get install xfe -y
+apt install dialog
+clear
+trap '' 2
+dialog --clear --backtitle "System Installation Type" --title "Choose Installation type:" --menu "Please select:" 10 45 3 1 "Minimal Installation 1.5GB" 2 "Full Installation 4GB" 2>temp
+# OK is pressed
+if [ "$?" = "0" ]
+then
+        _return=$(cat temp)
+ 
+        # Minimal is selected
+        if [ "$_return" = "1" ]
+        then
+        	echo 'Installing Minimal System '
+		      sleep 4
+    		  apt-get install mate-desktop-environment-core mate-terminal tightvncserver backbox-default-settings backbox-menu xfe -y
+        fi
+ 
+         # Full is selected
+        if [ "$_return" = "2" ]
+        then
+             echo 'Installing Full System '
+	           sleep 4
+             apt-get install mate-desktop-environment-core mate-terminal tightvncserver xfe gimp neofetch libreoffice backbox-default-settings backbox-menu -y
+    	       sudo apt update -y && sudo apt install wget -y && wget https://raw.githubusercontent.com/MobilinuxApp/Mobiconsole-CLI/master/Patches/librepatch.sh && bash librepatch.sh
+        fi
+ # Cancel is pressed
+else
+        echo "Cancel is pressed, Restarting The Menu......"
+	sleep 3
+	dialog --menu "Choose Installation type:" 10 40 3 1 "Minimal Installation 1.5GB" 2 "Full Installation 5GB" 2>temp
+fi
+ 
+# remove the temp file
+rm -f temp
+trap 2
 apt-get clean
 
 #Setup the necessary files
@@ -37,6 +70,8 @@ echo " "
 echo " "
 echo "Running vncserver-start"
 echo " "
+echo " "
+echo "**Note : Please note that you will need to enter view only password too while configuring VNC password to avoid connection errors."
 echo " "
 echo " "
 echo "To Kill VNC Server just run vncserver-stop"
