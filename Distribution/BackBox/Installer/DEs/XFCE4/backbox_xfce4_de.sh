4b293b46e1772eb633964eafbbc1464b242cf453
Chinmay Gokhale <64396758+MobilinuxApp@users.noreply.github.com>
2020-06-17 18:38:11 +0530

    Update backbox_xfce4_de.sh

--- Distribution/BackBox/Installer/DEs/XFCE4/backbox_xfce4_de.sh
+++ Distribution/BackBox/Installer/DEs/XFCE4/backbox_xfce4_de.sh
@@ -17,10 +17,9 @@ then
         then
                 echo 'Installing Minimal System '
 		sleep 4
-		rm -rf /var/lib/apt/lists/*
 		apt-get clean
-		apt-get update
-		apt-get install backbox-default-settings backbox-desktop backbox-menu xfce4 xfce4-terminal tightvncserver xfe -y
+		apt-get update --fix-missing
+		apt-get install backbox-default-settings backbox-desktop backbox-menu xfce4 xfce4-terminal tightvncserver xfe --fix-missing -y
     	
         fi
  
@@ -29,10 +28,9 @@ then
         then
             	echo 'Installing Full System '
 		sleep 4
-		rm -rf /var/lib/apt/lists/*
 		apt-get clean
-		apt-get update
-		apt-get install xfe backbox-default-settings backbox-desktop backbox-menu backbox-tools xfce4 xfce4-terminal beef-project metasploit-framework whatweb wpscan setoolkit tightvncserver xfce4-goodies gimp neofetch libreoffice -y
+		apt-get update --fix-missing
+		apt-get install xfe backbox-default-settings backbox-desktop backbox-menu backbox-tools xfce4 xfce4-terminal beef-project metasploit-framework whatweb wpscan setoolkit tightvncserver xfce4-goodies gimp neofetch libreoffice --fix-missing -y
     		sudo apt-get install openvas sqlite3
 		sudo openvas-launch sync
 		sudo openvas-launch start
