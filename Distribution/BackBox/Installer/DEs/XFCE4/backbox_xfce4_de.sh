041bb2e2f59a82a7f7dde6bbc480fa8f8f4432dd
Chinmay Gokhale <64396758+MobilinuxApp@users.noreply.github.com>
2020-06-17 16:08:30 +0530

    Update backbox_xfce4_de.sh

--- Distribution/BackBox/Installer/DEs/XFCE4/backbox_xfce4_de.sh
+++ Distribution/BackBox/Installer/DEs/XFCE4/backbox_xfce4_de.sh
@@ -3,8 +3,41 @@
 #Get the necessary components
 apt-mark hold udisks2
 [ ! -f /root/.parrot ] && apt-get update || echo "Parrot detected, not updating apt cache since that will break the whole distro"
-apt-get install xfce4 xfce4-terminal tigervnc-standalone-server gimp neofetch -y
-apt-get install xfe -y
+apt install dialog
+clear
+trap '' 2
+dialog --clear --backtitle "System Installation Type" --title "Choose Installation type:" --menu "Please select:" 10 45 3 1 "Minimal Installation 1.5GB" 2 "Full Installation 4GB" 2>temp
+# OK is pressed
+if [ "$?" = "0" ]
+then
+        _return=$(cat temp)
+ 
+        # Minimal is selected
+        if [ "$_return" = "1" ]
+        then
+                echo 'Installing Minimal System '
+		sleep 4
+    		apt-get install xfce4 xfce4-terminal tigervnc-standalone-server xfe -y
+        fi
+ 
+         # Full is selected
+        if [ "$_return" = "2" ]
+        then
+            	echo 'Installing Full System '
+		sleep 4
+		apt-get install xfe xfce4 xfce4-terminal tigervnc-standalone-server xfce4-goodies gimp neofetch libreoffice -y
+    		sudo apt update -y && sudo apt install wget -y && wget https://raw.githubusercontent.com/MobilinuxApp/Mobiconsole-CLI/master/Patches/librepatch.sh && bash librepatch.sh
+        fi
+ # Cancel is pressed
+else
+        echo "Cancel is pressed, Restarting The Menu......"
+	sleep 3
+	dialog --menu "Choose Installation type:" 10 40 3 1 "Minimal Installation 1.5GB" 2 "Full Installation 4GB" 2>temp
+fi
+ 
+# remove the temp file
+rm -f temp
+trap 2
 apt-get clean
 
 #Setup the necessary files
@@ -33,6 +66,8 @@ echo "You can connect to this address with a VNC Viewer you prefer"
 echo " "
 echo "Connect to this address will open a window with Xfce4 Desktop Environment"
 echo " "
+echo " "
+echo "**Note : Please note that you will need to enter view only password too while configuring VNC password to avoid connection errors."
 echo " "
 echo " "
 echo "Running vncserver-start"
