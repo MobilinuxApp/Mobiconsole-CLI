f1fb39b9c16ffe0ebc92b037c29ff547930ce9c4
Chinmay Gokhale <64396758+MobilinuxApp@users.noreply.github.com>
2020-07-11 12:01:42 +0530

    Update ubuntu_i3_wm.sh

--- Distribution/Ubuntu/Installer/WindowManager/i3/ubuntu_i3_wm.sh
+++ Distribution/Ubuntu/Installer/WindowManager/i3/ubuntu_i3_wm.sh
@@ -56,7 +56,7 @@ esac
 
 mkdir -p ~/.vnc
 
-wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/WM/wallpaper.jpg -O /usr/share/wallpaper.jpg
+wget https://raw.githubusercontent.com/Mobiconsole-CLI/Distribution/Ubuntu/Installer/WindowManager/wallpaper.jpg -O /usr/share/wallpaper.jpg
 echo "#!/bin/bash
 [ -r ~/.Xresources ] && xrdb ~/.Xresources
 export PULSE_SERVER=127.0.0.1
